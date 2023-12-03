b565084aa5641fc722b8ddcc5651099e766b660a
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-12-03 17:03:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const fileUpload = require('express-fileupload');
-const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const morgan =require('morgan')
@@ -15,12 +14,8 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 app.use(morgan('tiny'));
 
-app.use(cookieParser('CookingBlogSecure'));
-app.use(session({
-  secret: 'CookingBlogSecretSession',
-  saveUninitialized: true,
-  resave: true
-}));
+app.use(cookieParser(process.env.SECRET_KEY));
+
 app.use(flash());
 app.use(fileUpload());
 
